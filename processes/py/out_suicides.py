fdaa92e65988d05dbdaa33dde3117d48a14fb352
Sydney Melendres <sydneymelendres@gmail.com>
2024-05-31 12:15:16 +1000

    get final score per round

--- processes/py/out_suicides.py
+++ processes/py/out_suicides.py
@@ -10,7 +10,7 @@ df.columns = df.columns.str.strip()
 df = df[(df['killer'] != '<world>') & (df['killer'] != df['victim'])]
 
 # Save the adjusted dataframe back to a CSV file
-output_file_path = 'complete.csv'
+output_file_path = 'final_data\complete.csv'
 df.to_csv(output_file_path, index=False)
 
 output_file_path
